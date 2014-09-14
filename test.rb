dd9bdfcc3562eff40e2ee7a5cacf4d6fea7d47eb
Jared Breeden <jared.breeden@gmail.com>
2014-09-14 19:39:38 -0400

    Updating test.rb

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
-def test(desc, cond)
+def test(desc)
   print desc
-  if cond
+  if yield
     puts "Succeeded"
   else
     puts "Failed"
@@ -8,27 +8,40 @@ def test(desc, cond)
 end
 
 begin
-  test("RubydoTest class definition: ", Object.const_defined?(:RubydoTest) && RubydoTest.class == Class)
+  test "RubydoTest class definition: " do
+    Object.const_defined?(:RubydoTest) && RubydoTest.class == Class
+  end
   
-  ruby_do_test = RubydoTest.new
+  test "Instance method definition: " do
+    ruby_do_test = RubydoTest.new
+    ruby_do_test.methods.any? { |m| m.to_s == "test_method_returns_success"}
+  end
   
-  test("Instance method definition: ", ruby_do_test.methods.any? { |m| m.to_s == "test_method_returns_success"})
+  test "Instance method return value: " do
+    ruby_do_test = RubydoTest.new
+    result = ruby_do_test.test_method_returns_success
+    result == "success"
+  end
   
-  result = ruby_do_test.test_method_returns_success
+  test "NestClass definition: " do
+    RubydoTest.const_defined?(:NestedClass) && RubydoTest::NestedClass.class == Class
+  end
   
-  test("Instance method return value: ", result == "success")
+  test "Nested instance method return value: " do
+    nested_class_object = RubydoTest::NestedClass.new
+    result = nested_class_object.nested_class_method
+    result == "success"
+  end
   
-  test("NestClass definition: ", RubydoTest.const_defined?(:NestedClass) && RubydoTest::NestedClass.class == Class)
+  test "Re-opening Object class by VALUE: " do
+    obj = Object.new
+    obj.rubydo_monkey_patch_by_value == "success"
+  end
   
-  nested_class_object = RubydoTest::NestedClass.new
-  result = nested_class_object.nested_class_method
-  
-  test("Nested instance method return value: ", result == "success")
-  
-  obj = Object.new
-  
-  test("Re-opening Object class by VALUE: ", obj.rubydo_monkey_patch_by_value == "success")
-  test("Re-opening Object class by name: ", obj.rubydo_monkey_patch_by_name == "success")
+  test "Re-opening Object class by name: " do
+    obj = Object.new
+    obj.rubydo_monkey_patch_by_name == "success"
+  end
   
 rescue Exception => ex
   puts ex
